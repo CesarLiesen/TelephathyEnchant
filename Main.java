5cdfa7b033bdef8a0cfc26b30a0728101fbe3bb0
CesarLiesen <69520119+CesarLiesen@users.noreply.github.com>
2021-03-02 09:42:56 +0100

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,21 @@
+package me.Cesar.TelephathyEnchant;
+
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin{
+
+	
+	
+	@Override
+	public void onEnable() {
+
+	}
+
+	@Override
+	public void onDisable() {
+
+	}
+
+
+}
